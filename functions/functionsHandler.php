1cd15fe77e5c74705fea71227ca7892b105a670c
Jesperjensensvard <43741171+Jesperjensensvard@users.noreply.github.com>
2020-03-18 22:57:03 +0100

    init

---
+++ functions/functionsHandler.php
@@ -0,0 +1,2 @@
+<?php 
+require_once('basicfunc.php');
---
+++ theme/footer.php
@@ -0,0 +1,13 @@
+<!-- </body>
+</html> -->
+<h1>askdjlköasjdlöaksjdas</h1>
+<h1>askdjlköasjdlöaksjdas</h1>
+<h1>askdjlköasjdlöaksjdas</h1>
+<h1>askdjlköasjdlöaksjdas</h1>
+<h1>askdjlköasjdlöaksjdas</h1>
+<h1>askdjlköasjdlöaksjdas</h1>
+<h1>askdjlköasjdlöaksjdas</h1>
+<h1>askdjlköasjdlöaksjdasssss</h1>
+<h1>askdjlköasjdlöaksjdas</h1>
+<h1>askdjlköasjdlöaksjdas</h1>
+<h1>askdjlköasjdlöaksjdas</h1>
